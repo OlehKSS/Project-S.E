69eec42ab57356e134dd254b5b9662a9d03b40ab
Thomas DREVET <dreve2thom@hotmail.fr>
2016-12-15 13:42:35 +0100

    Modification on the Copyright

--- Source-code-3D-Scanner/Un-Optimized-Full-source-Code/Without-GUI/reconstructionproject.h
+++ Source-code-3D-Scanner/Un-Optimized-Full-source-Code/Without-GUI/reconstructionproject.h
@@ -1,18 +1,37 @@
 /*
- * Copyright (c) 2016, Maria del Carmen Moreno Genis, Omair Khalid and Thomas Drevet.
- * All rights reserved.
- *
- *This class was created to achieve 3D Reconstruction, as part of a project to make a 3D Scanner. It
- *takes a registered point cloud as the input and then outputs a watertight mesh which is suitable for
- *3D Printing.
- *
- *The 3D Reconstruction can be achieved by either one of the two methods:
- *Poisson 3D Reconstruction or Greedy Reconstruction.
- *
- *The user can change the default parameters for the 3D Reconstruction method through the
- *GUI to achieve desirable results.
- *
- */
+By downloading, copying, installing or using the software you agree to this license.
+If you do not agree to this license, do not download, install,
+copy or use the software.
+                          License Agreement
+            For Open Source Computer Vision and Robotics Project
+Copyright (C) 2016,
+      Moreno Genis Maria del Carmen (mc.genis2@gmail.com),
+      Khalid Omair (omair.khalid92@hotmail.com),
+      Drevet Thomas (dreve2thom@hotmail.fr),
+Third party copyrights are property of their respective owners.
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+  * Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+  * Neither the names of the copyright holders nor the names of the contributors
+    may be used to endorse or promote products derived from this software
+    without specific prior written permission.
+This software is provided by the copyright holders and contributors "as is" and
+any express or implied warranties, including, but not limited to, the implied
+warranties of merchantability and fitness for a particular purpose are disclaimed.
+In no event shall copyright holders or contributors be liable for any direct,
+indirect, incidental, special, exemplary, or consequential damages
+(including, but not limited to, procurement of substitute goods or services;
+loss of use, data, or profits; or business interruption) however caused
+and on any theory of liability, whether in contract, strict liability,
+or tort (including negligence or otherwise) arising in any way out of
+the use of this software, even if advised of the possibility of such damage.
+
+Refer to the standard.txt to have a deep understanding of variables names and definitions
+*/
 
 
 #ifndef RECONSTRUCTIONPROJECT_H
@@ -33,7 +52,48 @@
 
 #include <pcl/io/pcd_io.h>              /////////////////////DELETE !!!!
 #include <pcl/visualization/pcl_visualizer.h> ///////////////DELETE !!!!
+#include <pcl/io/ply_io.h>              /////////////////////DELETE !!!!
 
+
+
+/*
+  This class was created to achieve 3D Reconstruction, as part of a project to make a 3D Scanner. It
+  takes a registered point cloud as the input and then outputs a watertight mesh which is suitable for
+  3D Printing.
+
+  The 3D Reconstruction can be achieved by either one of the two methods:
+    -Poisson 3D Reconstruction
+    -Greedy Reconstruction.
+
+  The user can change the default parameters for the 3D Reconstruction method through the
+  GUI to achieve desirable results.
+
+  ///Constructor
+        @NULL :: Creating default file with default values for parameters
+  ///Destructor
+        @NULL :: Closing the file opened
+  ///Functions
+        reconstruction :: Called function in the software containing the other functions
+        getU_octree_depth :: Accessor for the u_octree_depth parameter
+        setU_octree_depth :: Mutator for the u_octree_depth parameter
+        getU_radius_search :: Accessor for the u_radius_search parameter
+        setU_radius_search :: Mutator for the u_radius_search parameter
+        getU_mu :: Accessor for the u_mu parameter
+        setU_mu :: Mutator for the u_mu parameter
+        getU_max_nearest_neighbor :: Accessor for the u_max_nearest_neighbor parameter
+        setU_max_nearest_neighbor :: Mutator for the u_max_nearest_neighbor parameter
+        getU_max_surface_angle :: Accessor for the u_max_surface_angle parameter
+        setU_max_surface_angle :: Mutator for the u_max_surface_angle parameter
+        getU_min_angle :: Accessor for the u_min_angle parameter
+        setU_min_angle :: Mutator for the u_min_angle parameter
+        getU_max_angle :: Accessor for the u_max_angle parameter
+        setU_max_angle :: Mutator for the u_max_angle parameter
+        getU_normal_consistency :: Accessor for the u_normal_consistency parameter
+        setU_normal_consistency :: Mutator for the u_normal_consistency parameter
+        normal_estimation :: Compute the normal estimation needed Poisson and the Greedy Triangulation
+        poisson_algorithm :: Create a 3D Mesh using the Poisson Algorithm from a point cloud
+        greedy_triangulation :: Create a 3D Mesh using the Greedy Triangulation method from a point cloud
+ */
 
 class ReconstructionProject
 {
